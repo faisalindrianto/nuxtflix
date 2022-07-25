8b44aa440476ed52e3932421a09bfcdba2e2192d
Faisal Indrianto <wwf051099@gmail.com>
2022-07-25 21:21:53 +0700

    enable dialog provider

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
   <n-config-provider :theme="darkTheme" :theme-overrides="themeOverrides">
     <n-message-provider>
-      <n-layout>
-        <n-layout-content>
-          <navbar />
-          <slot />
-        </n-layout-content>
-      </n-layout>
+      <n-dialog-provider>
+        <n-layout>
+          <n-layout-content>
+            <navbar />
+            <slot />
+          </n-layout-content>
+        </n-layout>
+      </n-dialog-provider>
     </n-message-provider>
   </n-config-provider>
 </template>
